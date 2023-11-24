71392c26b2b86533962571be8e04b1a5b2bd20df
AndreiNune <andreimain1224@gmail.com>
2023-11-24 14:43:35 -0300

    Iniciando css formularios

--- Acesso BD/Autoria/CAutores.php
+++ Acesso BD/Autoria/CAutores.php
@@ -2,59 +2,65 @@
 
 <head>
     <title>Cadastrar</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/formg.css">
 </head>
 
 <body>
-    <br>
-    <br>
-    <br>
-    <form name="cliente" method="POST" action="">
 
-        <b>Dados dos Autores</b>
+    <div class="wrapper">
 
-        <br>
-        <p> <input name="txtcod" type="number" size="10" maxlength="40" placeholder="Codigo do autor: ">
-        <p> <input name="txtnome" type="text" size="40" placeholder="Nome: "></p>
-        <p> <input name="txtsbnome" type="text" size="40" placeholder="Sobrenome: "></p>
-        <p> <input name="txtemail" type="text" size="30" placeholder="Email: "></p>
-        <p> <input name="txtnasc" type="date" size="8" placeholder="Nascimento: "></p>
-
-        <br>
-
-        <b> Opções: </b>
-        <br>
-        <input name="btnenviar" type="submit" value="Cadastrar"> &nbsp;&nbsp;
-        <input name="limpar" type="reset" value="Limpar">
-
-        <?php
-        extract($_POST, EXTR_OVERWRITE);
-        if (isset($btnenviar)) {
-            $Cod_autor = $_POST["txtcod"];
-            $NomeAutor = $_POST["txtnome"];
-            $Sobrenome = $_POST["txtsbnome"];
-            $Email = $_POST["txtemail"];
-            $Nasc = $_POST["txtnasc"];
-
-            include_once './model&conection/Autores.php';
-
-            $pro = new Autor();
-
-            $pro->setCod_autor($txtcod);
-            $pro->setNomeAutor($txtnome);
-            $pro->setSobrenome($txtsbnome);
-            $pro->setEmail($txtemail);
-            $pro->setNasc($txtnasc);
+        <h1>Cadastro de Autores</h1>
 
 
-            echo "<h3><br><br>" . $pro->salvar() . "</h3>";
+        <form name="cliente" method="POST" action="">
 
-        }
-        ?>
+            <b>Dados dos Autores</b>
 
-        <br>
-        <button><a href="menu.html"> Voltar ao menu</a></button>
-        <button><a href="LAutores.php"> Listagem </a></button>
+            <br>
+            <p> <input name="txtcod" type="number" size="10" maxlength="40" placeholder="Codigo do autor: ">
+            <p> <input name="txtnome" type="text" size="30" placeholder="Nome: "></p>
+            <p> <input name="txtsbnome" type="text" size="30" placeholder="Sobrenome: "></p>
+            <p> <input name="txtemail" type="text" size="30" placeholder="Email: "></p>
+            <p> <input name="txtnasc" type="date" size="8" placeholder="Nascimento: "></p>
+
+            <br>
+
+            <b> Opções: </b>
+            <br>
+            <input name="btnenviar" type="submit" value="Cadastrar"> &nbsp;&nbsp;
+            <input name="limpar" type="reset" value="Limpar">
+
+            <?php
+            extract($_POST, EXTR_OVERWRITE);
+            if (isset($btnenviar)) {
+                $Cod_autor = $_POST["txtcod"];
+                $NomeAutor = $_POST["txtnome"];
+                $Sobrenome = $_POST["txtsbnome"];
+                $Email = $_POST["txtemail"];
+                $Nasc = $_POST["txtnasc"];
+
+                include_once './model&conection/Autores.php';
+
+                $pro = new Autor();
+
+                $pro->setCod_autor($txtcod);
+                $pro->setNomeAutor($txtnome);
+                $pro->setSobrenome($txtsbnome);
+                $pro->setEmail($txtemail);
+                $pro->setNasc($txtnasc);
+
+
+                echo "<h3><br><br>" . $pro->salvar() . "</h3>";
+            }
+            ?>
+
+            <br>
+
+            <button><a href="menu.html"> Voltar ao menu</a></button>
+            <button><a href="LAutores.php"> Listagem </a></button>
+    </div>
+
+
 
 </body>
 
