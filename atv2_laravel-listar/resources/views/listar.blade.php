e740725de763926ecb1c3c803c333821e7bd0f07
AndreiNune <andreimain1224@gmail.com>
2024-03-06 13:49:23 -0300

    Completa

---
+++ atv2_laravel-listar/resources/views/listar.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <nav class="navbar navbar extend-lg bg-primary " data-bs-theme="dark">
+                    <div class="row">
+                        <div class="container-fluid">
+                            <label class="navbar-brand mb-2 p-2 h1 fw-medium">SISTEMA WEB</label>
+                            <label class="navbar-brand mb-2 h2 fw-light">Cadastrar</label>
+                            <label class="navbar-brand mb-2 h2 fw-light">Consultar</label>
+                        </div>
+                    </div>
+                </nav>
+
+                <div class="row">
+                    <div class="col">
+                        <p class="fs-2 fw-light">Cadastrar - Agendamento de Potenciais Clientes</p>
+                        <p class="fs-4 fw-light">Sistema Utilizado para agendamento de serviços.</p>
+                    </div>
+                </div>
+                <table class="table table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Origim</th>
+                            <th scope="col">Data de Contato</th>
+                            <th scope="col">Observação</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
